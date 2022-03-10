6c08d026387dbbc0943276e055642bdd5d2e4f1a
vportare <vportare@gmail.com>
2022-03-10 21:14:26 +0000

    Set up Leiningen project

---
+++ test/for_clojure_solutions/core_test.clj
@@ -0,0 +1,7 @@
+(ns for-clojure-solutions.core-test
+  (:require [clojure.test :refer :all]
+            [four-clojure-solutions.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
