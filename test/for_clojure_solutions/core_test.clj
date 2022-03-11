89d4a0e3a9899c6778ffe9a37afc5afcdf20c6c4
vportare <vportare@gmail.com>
2022-03-11 20:10:11 +0000

    Problem 1 - tests

--- test/for_clojure_solutions/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns for-clojure-solutions.core-test
-  (:require [clojure.test :refer :all]
-            [four-clojure-solutions.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
