0412b629d736405216db69d127e3b9484238976a
vportare <vportare@gmail.com>
2022-04-02 15:52:01 +0100

    Problem 19

--- src/four_clojure_solutions/019.clj
+++ src/four_clojure_solutions/019.clj
@@ -1,7 +1,21 @@
-(ns four-clojure-solutions.018)
+(ns four-clojure-solutions.019)
 
-;https://4clojure.oxal.org/#/problem/18
+; https://4clojure.oxal.org/#/problem/19
+; Write a function which returns the last element in a sequence.
+; Restrictions: last
 
 ;---------------------------------------------
 
-(= '(6 7) (filter #(> % 5) '(3 4 5 6 7)))
+(defn last-v1 [coll]
+  (first (reverse coll)))
+
+(defn last-v2 [coll]
+  (nth coll (- (count coll) 1) nil))
+
+;---------------------------------------------
+
+(= (last-v2 [1 2 3 4 5]) 5)
+(= (last-v2 '(5 4 3)) 3)
+(= (last-v2 ["b" "c" "d"]) "d")
+(= (last-v2 []) nil)
+(= (last-v2 nil) nil)
