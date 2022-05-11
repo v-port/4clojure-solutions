5687d5a1f92736c0dccdc272a2ff804d2faf5eb7
vportare <vportare@gmail.com>
2022-05-11 13:16:54 +0100

    Problem 40

--- src/four_clojure_solutions/040.clj
+++ src/four_clojure_solutions/040.clj
@@ -1,1 +1,18 @@
 (ns four-clojure-solutions.040)
+
+; https://4clojure.oxal.org/#/problem/40
+; Write a function which separates the items of
+; a sequence by an arbitrary value.
+; Restrictions - interpose
+
+;---------------------------------------------
+
+(defn interpose-v1 [sep coll]
+  (rest (interleave (repeat sep) coll)))
+
+;---------------------------------------------
+
+(comment
+  (= (interpose-v1 0 [1 2 3]) [1 0 2 0 3])
+  (= (apply str (interpose-v1 ", " ["one" "two" "three"])) "one, two, three")
+  (= (interpose-v1 :z [:a :b :c :d]) [:a :z :b :z :c :z :d]))
